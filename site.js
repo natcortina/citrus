6167f949200cd42438681c2cd7e2550de27a602c
natcortina <natcortinab@gmail.com>
2023-10-18 20:42:05 -0400

    added java

---
+++ site.js
@@ -0,0 +1,82 @@
+let fruits = {
+    lemon: {
+        color: "Yellow",
+        sweetness:"Low",
+        bitterness: "Low",
+        shape: 'Oval',
+        size: 'Small',
+        area: 'Worldwide',
+    },
+    pomelo: {
+        color: "Green",
+        sweetness: "High",
+        bitterness: "Low",
+        shape: "Round",
+        size: "Big",
+        area: "Asia",
+
+    }, 
+    orange:{ 
+        color: "Orange",
+        sweetness: "High",
+        bitterness: "Low",
+        shape: "Round",
+        size: "Medium",
+        area: "Worldwide",
+    },
+    bloodOrange:{
+        color: "Red",
+        sweetness: "High",
+        bitterness: "Low",
+        shape: "Round",
+        size: "Medium",
+        area: "Mediterranean",
+    },
+    lime: {
+        color: "Green",
+        sweetness: "Low",
+        bitterness: "Low",
+        shape: "Oval",
+        size: "Small",
+        area: "Florida",
+    },
+    mandarin: {
+        color: "Orange",
+        sweetness: "High",
+        bitterness:"Low",
+        shape: "Round",
+        size: "Small",
+        area: "Worldwide",
+    },
+    clementines: {
+        color: "Orange",
+        sweetness: "High",
+        bitterness: "Low",
+        shape: "Round",
+        size: "Small",
+        area: "Worldwide",
+    },
+    yuzu:{
+        color: "Yellow",
+        sweetness: "Low",
+        bitterness: "Low",
+        shape: "Round",
+        size: "Small",
+        area: "Asia",
+    },
+    grapefruit: {
+        color: "Pink",
+        sweetness: "Low",
+        bitterness: "High",
+        shape: "Round",
+        size: "Large",
+        area: "Worldwide",
+    }
+
+    }
+
+  
+    console.log(Object.keys(fruits.lemon));
+    console.log(Object.keys(fruits.lime));
+    
+
